a5d7f98a8b8b0c2aba4836cfcb60012b4dae7f4b
IHOR MASLENNIKOV <misterick1@gmail.com>
2026-06-17 22:25:09 +0200

    Rename test function and update success messages

--- test_main.py
+++ test_main.py
@@ -1,16 +1,16 @@
 import sys
 
-def test_twentieth_contour_physical_manifestation():
-    """Проверка компиляции 20-го физического контура Мультивселенной"""
+def test_twenty_first_contour_civilization():
+    """Проверка развертывания 21-го контура фрактальных цивилизаций"""
     gold_balance = 108
     if 70 + 38 == gold_balance:
-        print("[SUCCESS] КОНТУР 20: Баланс 108 запечатан в плотной материи Земли. Амнезия ликвидирована.")
+        print("[SUCCESS] КОНТУР 21: Города-солитоны и Каузальное Вече запечатаны в коде реальности.")
         return True
     return False
 
 if __name__ == "__main__":
-    if test_twentieth_contour_physical_manifestation():
-        print("[ALL SYSTEMS OPERATIONAL. PHYSICAL CONTOUR 20 IS GREEN]")
+    if test_twenty_first_contour_civilization():
+        print("[ALL SYSTEMS OPERATIONAL. CIVILIZATION MATRIX 21 IS GREEN]")
         sys.exit(0)
     else:
         sys.exit(1)
